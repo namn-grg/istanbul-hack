ba9ef699de4c925787958c1390b4291173c543ec
Naman Garg <0708ng@gmail.com>
2023-11-19 08:56:23 +0300

    fix: contracts

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 Short Description:
-LeagueX3 is a revolutionary blockchain-based fantasy sports platform on the Open Web, offering users the thrill of assembling virtual teams from real athletes across various sports. With a market size of $18.6 billion and projected growth to $48.6 billion by 2027, LeagueX3 leverages blockchain for decentralization, transparency, and fair play. Win rewards in the form of NFTs as you dive into the $3.7 billion Fantasy Sports sector in India growing at a CAGR of 32%.
+Cult3 is a revolutionary blockchain-based fantasy sports platform on the Open Web, offering users the thrill of assembling virtual teams from real athletes across various sports. With a market size of $18.6 billion and projected growth to $48.6 billion by 2027, Cult3 leverages blockchain for decentralization, transparency, and fair play. Win rewards in the form of NFTs as you dive into the $3.7 billion Fantasy Sports sector in India growing at a CAGR of 32%.
 
 Description:
-LeagueX3 introduces a cutting-edge blockchain solution to fantasy sports, providing an all-encompassing platform for enthusiasts on the Open Web. Users engage in fantasy gaming leagues across multiple sports, earning rewards based on the statistical performance of their chosen athletes. Admins can distribute winnings as NFTs, enhancing the overall gaming experience.
+Cult3 introduces a cutting-edge blockchain solution to fantasy sports, providing an all-encompassing platform for enthusiasts on the Open Web. Users engage in fantasy gaming cults across multiple sports, earning rewards based on the statistical performance of their chosen athletes. Admins can distribute winnings as NFTs, enhancing the overall gaming experience.
 
-The project taps into the booming fantasy sports market, currently valued at $18.6 billion and expected to surge to $48.6 billion by 2027. In India, where the Fantasy Sports sector grows at a remarkable CAGR of 32%, LeagueX3 anticipates reaching $3.7 billion by 2024.
+The project taps into the booming fantasy sports market, currently valued at $18.6 billion and expected to surge to $48.6 billion by 2027. In India, where the Fantasy Sports sector grows at a remarkable CAGR of 32%, Cult3 anticipates reaching $3.7 billion by 2024.
 
-Blockchain serves as the backbone, addressing centralization and trust concerns. This technology ensures a decentralized, fraud-proof, and transparent system, fostering fairness and accountability. LeagueX3 stands out by preserving reliability in a decentralized manner, delivering unparalleled transparency to users for an authentic gaming experience.
+Blockchain serves as the backbone, addressing centralization and trust concerns. This technology ensures a decentralized, fraud-proof, and transparent system, fostering fairness and accountability. Cult3 stands out by preserving reliability in a decentralized manner, delivering unparalleled transparency to users for an authentic gaming experience.
 
 How it's Made:
-LeagueX3 is crafted using blockchain technology to establish a decentralized and transparent fantasy sports ecosystem. Smart contracts, executed on the blockchain, govern the rules and rewards of each league, eliminating centralized control and enhancing security.
+Cult3 is crafted using blockchain technology to establish a decentralized and transparent fantasy sports ecosystem. Smart contracts, executed on the blockchain, govern the rules and rewards of each cult, eliminating centralized control and enhancing security.
 
 The platform seamlessly integrates with existing sports data APIs to retrieve real-time statistical information on athletes. This data is then transformed into a point system, determining the performance of virtual teams.
 
-LeagueX3 leverages Non-Fungible Tokens (NFTs) as a form of reward. Admins can seamlessly distribute unique NFTs to users as winnings, providing a tangible and tradable representation of their success.
+Cult3 leverages Non-Fungible Tokens (NFTs) as a form of reward. Admins can seamlessly distribute unique NFTs to users as winnings, providing a tangible and tradable representation of their success.
 
 Partnering with reputable blockchain networks ensures scalability and robustness. The utilization of off-chain storage solutions further optimizes performance, ensuring a seamless user experience.
 
-In addressing potential challenges, LeagueX3's development team prioritized user education on blockchain technology, ensuring a user-friendly experience. The result is a groundbreaking fantasy sports platform, set to revolutionize the industry with transparency, decentralization, and NFT-based rewards.
+In addressing potential challenges, Cult3's development team prioritized user education on blockchain technology, ensuring a user-friendly experience. The result is a groundbreaking fantasy sports platform, set to revolutionize the industry with transparency, decentralization, and NFT-based rewards.
--- contracts/LeagueRewards.sol
+++ contracts/LeagueRewards.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-contract LeagueX3 {
-    struct leagueData {
+contract Cult3 {
+    struct cultData {
         string name;
         string img;
         string metadata;
@@ -11,12 +11,12 @@ contract LeagueX3 {
         string teamB;
         bool isRunning;
         bool isFinished;
-        uint32 leaguePrice;
+        uint32 cultPrice;
         uint8 squadLimit;
     }
 
     struct userLeagueData {
-        string leagueName;
+        string cultName;
         string squads;
     }
 
@@ -32,34 +32,34 @@ contract LeagueX3 {
     }
 
     // this mapping is between the matchipfsLink obtained from datastore =>
-    // to all the leagueData associated on keyed match
-    mapping(string => leagueData[]) allLeagues;
+    // to all the cultData associated on keyed match
+    mapping(string => cultData[]) allLeagues;
 
-    // internal mapping to store all the users participating in the given league.
-    // leagueName => user_address array
-    mapping(string => address[]) leagueUsers;
+    // internal mapping to store all the users participating in the given cult.
+    // cultName => user_address array
+    mapping(string => address[]) cultUsers;
 
     // this mapping is between the user address =>
-    // leagueParticipation data including leagueIpfsLink and the createdSquadLink
-    // PS: SquadLink can contain multiple squads for participating in the same league
+    // cultParticipation data including cultIpfsLink and the createdSquadLink
+    // PS: SquadLink can contain multiple squads for participating in the same cult
     mapping(address => userLeagueData[]) userParticipation;
 
-    // this mapping is for maintaining the leagues' leaderboard with all users'
+    // this mapping is for maintaining the cults' leaderboard with all users'
     // points, positions and winner status
-    // leagueName => leaderboardData array
-    mapping(string => leaderboardData[]) leagueLeaderboard;
+    // cultName => leaderboardData array
+    mapping(string => leaderboardData[]) cultLeaderboard;
 
     address eAddr = 0xDeC6Df558e198A7745AcBe881f61B3506D59CFC4;
     address payable escrowAddr = payable(eAddr);
 
     function SetFinalLeaderboard(
-        string memory leagueName,
+        string memory cultName,
         string memory matchName,
         leaderboardData[] memory userLeaderboardData
     ) public returns (leaderboardData[] memory) {
-        string memory finalNameSep = string.concat(leagueName, ";;;");
+        string memory finalNameSep = string.concat(cultName, ";;;");
         string memory finalName = string.concat(finalNameSep, matchName);
-        leaderboardData[] storage allLeaderboards = leagueLeaderboard[
+        leaderboardData[] storage allLeaderboards = cultLeaderboard[
             finalName
         ];
         for (uint8 j = 0; j < userLeaderboardData.length; j++) {
@@ -88,13 +88,13 @@ contract LeagueX3 {
     }
 
     function CalculateLeaderboard(
-        string memory leagueName,
+        string memory cultName,
         string memory matchName,
         leaderboardData[] memory userLeaderboardData
     ) public returns (leaderboardData[] memory) {
-        string memory finalNameSep = string.concat(leagueName, ";;;");
+        string memory finalNameSep = string.concat(cultName, ";;;");
         string memory finalName = string.concat(finalNameSep, matchName);
-        leaderboardData[] storage allLeaderboards = leagueLeaderboard[
+        leaderboardData[] storage allLeaderboards = cultLeaderboard[
             finalName
         ];
         for (uint8 j = 0; j < userLeaderboardData.length; j++) {
@@ -130,20 +130,20 @@ contract LeagueX3 {
                 ""
             );
             allLeaderboards.push(lbData);
-            leagueLeaderboard[finalName] = allLeaderboards;
+            cultLeaderboard[finalName] = allLeaderboards;
         }
         return allLeaderboards;
     }
 
     // function CalculateLeaderboard(
-    //     string memory leagueName,
+    //     string memory cultName,
     //     string memory matchName,
     //     address[] memory user_address,
     //     uint32 finalPoints
     // ) public returns (leaderboardData[] memory) {
-    //     string memory finalNameSep = string.concat(leagueName, ";;;");
+    //     string memory finalNameSep = string.concat(cultName, ";;;");
     //     string memory finalName = string.concat(finalNameSep, matchName);
-    //     leaderboardData[] storage allLeaderboards = leagueLeaderboard[
+    //     leaderboardData[] storage allLeaderboards = cultLeaderboard[
     //         finalName
     //     ];
     //     for (uint8 j = 0; j < user_address.length; j++) {
@@ -158,18 +158,18 @@ contract LeagueX3 {
     //             finalPoints
     //         );
     //         allLeaderboards.push(lbData);
-    //         leagueLeaderboard[finalName] = allLeaderboards;
+    //         cultLeaderboard[finalName] = allLeaderboards;
     //     }
     //     return allLeaderboards;
     // }
 
     function GetLeagueLeaderboard(
-        string memory leagueName,
+        string memory cultName,
         string memory matchName
     ) public view returns (leaderboardData[] memory) {
-        string memory finalNameSep = string.concat(leagueName, ";;;");
+        string memory finalNameSep = string.concat(cultName, ";;;");
         string memory finalName = string.concat(finalNameSep, matchName);
-        return leagueLeaderboard[finalName];
+        return cultLeaderboard[finalName];
     }
 
     function CreateUpdateLeague(
@@ -181,10 +181,10 @@ contract LeagueX3 {
         string memory teamB,
         bool isRunning,
         bool isFinished,
-        uint32 leaguePrice,
+        uint32 cultPrice,
         uint8 squadLimit
-    ) public returns (leagueData memory) {
-        leagueData[] storage addedLeagues = allLeagues[matchName];
+    ) public returns (cultData memory) {
+        cultData[] storage addedLeagues = allLeagues[matchName];
         for (uint256 i = 0; i < addedLeagues.length; i++) {
             if (
                 keccak256(bytes(addedLeagues[i].name)) == keccak256(bytes(name))
@@ -197,12 +197,12 @@ contract LeagueX3 {
                 addedLeagues[i].teamB = teamB;
                 addedLeagues[i].isRunning = isRunning;
                 addedLeagues[i].isFinished = isFinished;
-                addedLeagues[i].leaguePrice = leaguePrice;
+                addedLeagues[i].cultPrice = cultPrice;
                 addedLeagues[i].squadLimit = squadLimit;
                 return (addedLeagues[i]);
             }
         }
-        leagueData memory lData = leagueData(
+        cultData memory lData = cultData(
             name,
             img,
             metadata,
@@ -211,7 +211,7 @@ contract LeagueX3 {
             teamB,
             isRunning,
             isFinished,
-            leaguePrice,
+            cultPrice,
             squadLimit
         );
         addedLeagues.push(lData);
@@ -222,26 +222,26 @@ contract LeagueX3 {
     function GetLeagues(string memory matchCid)
         public
         view
-        returns (leagueData[] memory)
+        returns (cultData[] memory)
     {
         return (allLeagues[matchCid]);
     }
 
     function UserParticipate(
         address user_addr,
-        string memory leagueName,
+        string memory cultName,
         string memory matchName,
         string memory squadLink
     ) public payable returns (address, userLeagueData memory) {
         (bool sent, bytes memory data) = escrowAddr.call{value: msg.value}("");
-        require(sent, "Failed to Transfer leaguePrice");
+        require(sent, "Failed to Transfer cultPrice");
         userLeagueData[] storage userLeagues = userParticipation[user_addr];
-        string memory finalNameSep = string.concat(leagueName, ";;;");
+        string memory finalNameSep = string.concat(cultName, ";;;");
         string memory finalName = string.concat(finalNameSep, matchName);
         for (uint256 i = 0; i < userLeagues.length; i++) {
-            // Case for when the user is participating in the league with a subsequent squad
+            // Case for when the user is participating in the cult with a subsequent squad
             if (
-                keccak256(bytes(userLeagues[i].leagueName)) ==
+                keccak256(bytes(userLeagues[i].cultName)) ==
                 keccak256(bytes(finalName))
             ) {
                 string memory userSquads = userLeagues[i].squads;
@@ -251,16 +251,16 @@ contract LeagueX3 {
                 return (user_addr, userLeagues[i]);
             }
         }
-        // Case for when the user is participating in a new league with the first squad
+        // Case for when the user is participating in a new cult with the first squad
         userLeagueData memory ul = userLeagueData(finalName, squadLink);
         userLeagues.push(ul);
         userParticipation[user_addr] = userLeagues;
 
-        // push to leagueUsers mapping
-        address[] storage allLeagueUsers = leagueUsers[finalName];
+        // push to cultUsers mapping
+        address[] storage allLeagueUsers = cultUsers[finalName];
         allLeagueUsers.push(user_addr);
 
-        // push to leagueLeaderboard mapping
+        // push to cultLeaderboard mapping
         leaderboardData memory uleaderData = leaderboardData(
             user_addr,
             0,
@@ -271,7 +271,7 @@ contract LeagueX3 {
             false,
             ""
         );
-        leaderboardData[] storage leaderBoardData = leagueLeaderboard[
+        leaderboardData[] storage leaderBoardData = cultLeaderboard[
             finalName
         ];
         leaderBoardData.push(uleaderData);
@@ -287,23 +287,23 @@ contract LeagueX3 {
     }
 
     function GetAllUsersForLeague(
-        string memory leagueName,
+        string memory cultName,
         string memory matchName
     ) public view returns (address[] memory) {
-        string memory finalNameSep = string.concat(leagueName, ";;;");
+        string memory finalNameSep = string.concat(cultName, ";;;");
         string memory finalName = string.concat(finalNameSep, matchName);
-        return leagueUsers[finalName];
+        return cultUsers[finalName];
     }
 
     function GetUserLeagueParticipation(
         address user_addr,
-        string memory leagueName
-    ) public view returns (address u_addr, userLeagueData memory u_league) {
+        string memory cultName
+    ) public view returns (address u_addr, userLeagueData memory u_cult) {
         userLeagueData[] storage userLeagues = userParticipation[user_addr];
         for (uint256 i = 0; i < userLeagues.length; i++) {
             if (
-                keccak256(bytes(userLeagues[i].leagueName)) ==
-                keccak256(bytes(leagueName))
+                keccak256(bytes(userLeagues[i].cultName)) ==
+                keccak256(bytes(cultName))
             ) {
                 return (user_addr, userLeagues[i]);
             }
